ca50a467ebf7e7578143e9195be9191700258417
unknown <bryansilvamedina@gmail.com>
2024-07-14 17:05:13 -0400

    testeo1

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-
+from . import views
 from  ProyectoWebApp.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name="Home"),
-    
+    path('auto/', views.lista_autos, name='lista_autos'),
+    path('auto/nuevo/', views.crear_auto, name='crear_auto'),
+    path('auto/<int:pk>/', views.detalle_auto, name='detalle_auto'),
+    path('auto/<int:pk>/editar/', views.actualizar_auto, name='actualizar_auto'),
+    path('auto/<int:pk>/eliminar/', views.eliminar_auto, name='eliminar_auto'),
     
 
 
--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
 from carro.carro import Carro
+from .forms import AutoForm 
+from .models import Auto 
 
 
 
@@ -14,10 +18,43 @@ def home(request):
     return render(request,"ProyectoWebApp/home.html")
 
 
+def lista_autos(request):
+    autos = Auto.objects.all()
+    return render(request, 'lista_autos.html', {'autos': autos})
+
+def detalle_auto(request, auto_id):
+    auto = get_object_or_404(Auto, pk=auto_id)
+    return render(request, 'detalle_auto.html', {'auto': auto})
+
+def crear_auto(request):
+    if request.method == 'POST':
+        form = AutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autos')
+    else:
+        form = AutoForm()
+    return render(request, 'crear_auto.html', {'form': form})
 
 
-"giga"
+def actualizar_auto(request, pk):
+    auto = get_object_or_404(Auto, pk=pk)
+    if request.method == 'POST':
+        form = AutoForm(request.POST, instance=auto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autos')
+    else:
+        form = AutoForm(instance=auto)
+    
+    return render(request, 'actualizar_auto.html', {'form': form})
 
-
+def eliminar_auto(request, pk):
+    auto = get_object_or_404(Auto, pk=pk)
+    if request.method == 'POST':
+        auto.delete()
+        return redirect('lista_autos')
+    # Puedes manejar una solicitud GET para confirmar la eliminación, si es necesario
+    return render(request, 'eliminar_auto.html', {'auto': auto})
 
 
